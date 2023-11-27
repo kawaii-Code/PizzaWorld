4cfd2bef751bed7a9e93fb7bb4399b247a498ada
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-11-27 20:19:59 +0300

    Добавил логгер 📃

---
+++ Utilities/Debug.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.Chat;
+using Terraria.Localization;
+
+namespace ChubK.Utilities;
+
+public static class Debug
+{
+    public static void Log(string message)
+    {
+        Message(message, Color.White);
+    }
+
+    public static void LogWarning(string message)
+    {
+        Message(message, Color.Orange);
+    }
+
+    public static void LogError(string message)
+    {
+        Message(message, Color.Red);
+    }
+
+    private static void Message(string message, Color color)
+    {
+        ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral(message), color);
+    }
+}
