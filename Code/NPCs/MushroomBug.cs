ba33f5244f3340e4ec95d8b22d67640484de2254
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-11-28 22:54:53 +0300

    Добавил мирных птичку и гриб 🕊

---
+++ Code/NPCs/MushroomBug.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PizzaWorld.Code.NPCs;
+
+public class MushroomBug : ModNPC
+{
+    public override void SetStaticDefaults()
+    {
+        Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.Buggy];
+        Main.npcCatchable[Type] = true;
+        
+        NPCID.Sets.CountsAsCritter[Type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        NPC.CloneDefaults(NPCID.Buggy);
+        NPC.width = 16;
+        NPC.height = 12;
+        AnimationType = NPCID.Buggy;
+    }
+}
---
+++ Code/NPCs/PizzaBird.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+
+namespace PizzaWorld.Code.NPCs;
+
+public class PizzaBird : ModNPC
+{
+    public override void SetStaticDefaults()
+    {
+        Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.Bird];
+        Main.npcCatchable[Type] = true;
+
+        NPCID.Sets.CountsAsCritter[Type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        NPC.CloneDefaults(NPCID.Bird);
+        AnimationType = NPCID.Bird;
+    }
+
+    public override float SpawnChance(NPCSpawnInfo spawnInfo)
+    {
+        return SpawnCondition.OverworldDay.Chance * 0.1f;
+    }
+}
