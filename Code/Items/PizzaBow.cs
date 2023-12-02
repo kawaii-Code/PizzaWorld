70215f7f421779e8c5a94e0090fb9f4cfaf6ff01
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-12-02 20:09:41 +0300

    Добавил лук 🏹

---
+++ Code/Items/PizzaBow.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PizzaWorld.Code.Items;
+
+public class PizzaBow : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.CloneDefaults(ItemID.PlatinumBow);
+        Item.damage = 15;
+        Item.crit = 7;
+        Item.useTime = 25;
+        Item.value = Silver(25);
+
+        int Silver(int value)
+        {
+            return 100 * value;
+        }
+    }
+}
