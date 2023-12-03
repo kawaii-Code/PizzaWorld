cf2beb30204a0e7821711a8aeef4981a40d0383f
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-12-03 20:14:34 +0300

    Добавил йо-йо 🪀

---
+++ Code/Items/PizzaYoyo.cs
@@ -0,0 +1,22 @@
+﻿using PizzaWorld.Code.Projectiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PizzaWorld.Code.Items;
+
+public class PizzaYoyo : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        ItemID.Sets.Yoyo[Type] = true;
+        ItemID.Sets.GamepadExtraRange[Type] = 10;
+        ItemID.Sets.GamepadSmartQuickReach[Type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.CloneDefaults(ItemID.Rally);
+        Item.shoot = ModContent.ProjectileType<PizzaYoyoProjectile>();
+        Item.damage = 16;
+    }
+}
---
+++ Code/Projectiles/PizzaYoyoProjectile.cs
@@ -0,0 +1,25 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PizzaWorld.Code.Projectiles;
+
+public class PizzaYoyoProjectile : ModProjectile
+{
+    public override void SetStaticDefaults()
+    {
+        ProjectileID.Sets.YoyosLifeTimeMultiplier[Type] = 6f;
+        ProjectileID.Sets.YoyosMaximumRange[Type] = 200f;
+        ProjectileID.Sets.YoyosTopSpeed[Type] = 13f;
+    }
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 16;
+        Projectile.height = 16;
+
+        Projectile.aiStyle = ProjAIStyleID.Yoyo;
+        Projectile.friendly = true;
+        Projectile.DamageType = DamageClass.MeleeNoSpeed;
+        Projectile.penetrate = -1;
+    }
+}
