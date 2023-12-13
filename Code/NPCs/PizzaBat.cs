9688a0c7c8c3efbd35bef8da2c3619006d8a164f
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-12-13 21:34:02 +0300

    Добавил Пиццевую летучую мышь 🦇🍕

---
+++ Code/NPCs/PizzaBat.cs
@@ -0,0 +1,26 @@
+﻿using PizzaWorld.Code.Systems;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PizzaWorld.Code.NPCs;
+
+public class PizzaBat : ModNPC
+{
+    public override void SetStaticDefaults()
+    {
+        Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.JungleBat];
+    }
+
+    public override void SetDefaults()
+    {
+        NPC.CloneDefaults(NPCID.JungleBat);
+        AnimationType = NPCID.JungleBat;
+        AIType = NPCID.JungleBat;
+    }
+
+    public override float SpawnChance(NPCSpawnInfo spawnInfo)
+    {
+        return spawnInfo.Player.InModBiome<PizzaBiome>() ? 0.25f : 0.0f;
+    }
+}
